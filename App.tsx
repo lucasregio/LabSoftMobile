9621ae6486e45287050c3ece8608af2953e091d4
Lucas Regio <souzaregiolucas@gmail.com>
2021-10-07 10:37:45 -0300

    Revert "Merge pull request #8 from tallessiqueira/develop"
    This reverts commit daf8fb22b93d57735e3298536a0a7875c363d2d3, reversing
    changes made to aba346a2424f86f738c833da0922b3d4d197a7cb.

--- App.tsx
+++ App.tsx
@@ -1,34 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import AppLoading from 'expo-app-loading';
-
-import Routes from './src/routes';
-
-import { FredokaOne_400Regular, useFonts } from '@expo-google-fonts/fredoka-one';
-import { Nunito_400Regular } from '@expo-google-fonts/nunito';
-import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './src/contexts/auth';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  
-  const [fontsLoaded] = useFonts({
-    FredokaOne_400Regular,
-    Nunito_400Regular
-  });
-
-  
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
   return (
-    <>
-      <StatusBar style="dark" backgroundColor="transparent" translucent/>
-      <NavigationContainer>
-        <AuthProvider>
-          <Routes/>
-        </AuthProvider>
-      </NavigationContainer>
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
