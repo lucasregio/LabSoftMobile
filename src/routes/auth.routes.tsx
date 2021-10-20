55af873f6b7e93cc251a95ea10e2e6513c0b62aa
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2021-10-19 22:01:07 -0300

    feat: select athletic navigation

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -4,13 +4,15 @@ import Login from '../pages/Login';
 import ForgotPassword from '../pages/ForgotPassword';
 import colors from '../styles/colors';
 import Register from '../pages/Register';
+import Athletic, { AthleticScreenProp } from '../pages/Athletic';
 
 const AuthStack = createStackNavigator<AuthStackParamList>();
 
 export type AuthStackParamList = {
     Login: undefined;
     ForgotPassword: undefined;
-    //Register: undefined;
+    Athletic: any;
+    Register: undefined;
 };
 
 const stackOptions: StackNavigationOptions = {
@@ -25,6 +27,7 @@ const AuthRoutes = () => {
         <AuthStack.Navigator initialRouteName="Login" screenOptions={stackOptions}>
             <AuthStack.Screen name="Login" component={Login}/>
             <AuthStack.Screen name="ForgotPassword" component={ForgotPassword}/>
+            <AuthStack.Screen name="Athletic" component={Athletic}/>
             <AuthStack.Screen name="Register" component={Register}/>
             {/*<AuthStack.Screen name="" component={}/>*/}
         </AuthStack.Navigator>
