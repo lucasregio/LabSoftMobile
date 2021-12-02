a6ced618566346baf8b0bdf716f15527655f8090
Rafael Moreira <rafaelsmoreiras@gmail.com>
2021-12-02 10:11:17 -0300

    refact: adicionado alteração no interceptors para tratar erros nos metodos de chamada da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,24 +1,22 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import { useAuth } from '../contexts/auth';
 
 const api = axios.create({
     baseURL:"http://178.238.233.159:5555"
 })
 
-api.interceptors.response.use(response => response, error => {
-    const status = error.response ? error.response.status : null;
+api.interceptors.response.use((response) => {
+    console.log("Response Interceptor!");
+    return response
 
-    // 401 Unauthorized
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
-    if (status === 401) {
-        console.log("Token Invalido!")
-        //AsyncStorage.clear().then(() => {
-            //api.defaults.headers.common.Authorization = '';
-        //})
-    }
+    },(error) => {
+        const status = error.response ? error.response.status : null;    
+        console.log("Error Interceptor!");
+        //console.log("Error data:",error.response.data);
+        if (status === 401) {
+            console.log("Token Invalido!")
+        }
 
-    return error;
-})
+        return Promise.reject(error);
+}); 
 
 export default api;
